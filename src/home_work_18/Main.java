8be9be60afd73138619b686424e4de2e38e7bf09
KorbutViacheslav <minowaman777@gmail.com>
2022-12-14 08:59:38 +0200

    Add homework 18.

--- src/home_work_18/Main.java
+++ src/home_work_18/Main.java
@@ -3,34 +3,33 @@ package home_work_18;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
 
-        Box<Apple> appleBox=new Box<>();
+        Box<Apple> appleBox = new Box<>();
         appleBox.add(new Apple());
         appleBox.add(new Apple());
-        appleBox.addAll(new Apple(),new Apple(),new Apple(),new Apple());
+        appleBox.addAll(new Apple(), new Apple(), new Apple(), new Apple());
 
-        Box<Orange> orangeBox=new Box<>();
+        Box<Orange> orangeBox = new Box<>();
         orangeBox.add(new Orange());
         orangeBox.add(new Orange());
         orangeBox.add(new Orange());
         orangeBox.addAll(new Orange());
         System.out.println(orangeBox.compare(appleBox));
 
-        Box<Orange> orangeBox1=new Box<>();
-        orangeBox1.addAll(new Orange(),new Orange(),new Orange());
+        Box<Orange> orangeBox1 = new Box<>();
+        orangeBox1.addAll(new Orange(), new Orange(), new Orange());
         orangeBox.merge(orangeBox1);
         System.out.println(orangeBox.getWeight());
         System.out.println(orangeBox.getFruitsList());
 
-
+        Fruit[] fruits = orangeBox.getFruitsList().toArray(Fruit[]::new);
+        System.out.println(toList(fruits));
     }
 
-    public static List<?> toList(Objects[] objects) {
-        List<? extends Objects> list = new ArrayList<>(Arrays.asList(objects));
-        return list;
+    public static <T> List<T> toList(T[] objects) {
+        return new ArrayList<>(Arrays.asList(objects));
     }
 }
